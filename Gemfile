daad5f16f90a62b5e5e69bfa6bc9bba95088b4e0
pujasodha <pujasodha@gmail.com>
2019-10-31 21:15:43 -0700

    create dummy data / seeds

--- Gemfile
+++ Gemfile
@@ -73,5 +73,8 @@ group :test do
   gem 'database_cleaner'
 end
 
+# generate dummy text 
+gem 'faker'
+
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,56 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
+
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+def seed_users
+    user_id = 0 
+    10.times do
+        User.create(
+            name: "test#{user_id}",
+            email: "test#{user_id}@test.com",
+            password: "123456",
+            password_confirmation: "123456"
+        )
+        user_id = user_id + 1
+    end
+end 
+
+def seed_categories
+    hobby = ['Crafts', 'Basketball', 'Soccer', 'Photography', 'Hiking', 'Reading', 'Other']
+    study = ['Visual Arts', 'Physics', 'Mathematics', 'Computer Science', 'Biology', 'Health Science', 'Business', 'Economics', 'Language', 'Other']
+    team = ['Development', 'Focus', 'Play']
+
+    hobby.each do |name|
+        Category.create(branch: 'hobby', name: name)
+    end 
+
+    study.each do |name|
+        Category.create(branch: 'study', name: name)
+    end
+
+    team.each do |name|
+        Category.create(branch: 'study', name: name)
+    end
+end 
+
+def seed_posts
+    categories = Category.all
+
+    categories.each do |category| 
+        5.times do
+            Post.create(
+                title: Faker::Lorem.sentences[0],
+                content: Faker::Lorem.sentences[0],
+                user_id: rand(1..9),
+                category_id: category.id
+            )
+        end
+    end
+end 
+
+seed_users
+seed_categories
+seed_posts
