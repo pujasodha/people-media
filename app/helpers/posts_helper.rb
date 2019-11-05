7ea77ab786b57998d7271ba4c5ed0c79580b7f17
pujasodha <pujasodha@gmail.com>
2019-11-05 15:36:40 -0800

    search form rendered (no function - just visually)

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -20,4 +20,16 @@ module PostsHelper
         @posts.empty? ? 'posts/branch/no_posts' : 'shared/empty_partial'
     end
 
+    def post_format_partial_path
+        current_page?(root_path) ? 'posts/post/home_page' : 'posts/post/branch_page'
+    end
+
+     def category_field_partial_path
+    if params[:category].present?
+      'posts/branch/search_form/category_field'
+    else
+      'shared/empty_partial'
+    end
+  end
+
 end
