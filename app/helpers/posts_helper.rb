98e4136ab7be3a6779b24b1b33098e379bdd8f9e
pujasodha <pujasodha@gmail.com>
2019-11-05 23:43:03 -0800

    create new posts (modified home page too)

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -16,20 +16,22 @@ module PostsHelper
         end
     end
 
-    def no_posts_partial_path
-        @posts.empty? ? 'posts/branch/no_posts' : 'shared/empty_partial'
+    def no_posts_partial_path(posts)
+        posts.empty? ? 'posts/shared/no_posts' : 'shared/empty_partial'
     end
 
     def post_format_partial_path
         current_page?(root_path) ? 'posts/post/home_page' : 'posts/post/branch_page'
     end
 
-     def category_field_partial_path
-    if params[:category].present?
-      'posts/branch/search_form/category_field'
-    else
-      'shared/empty_partial'
+    def category_field_partial_path
+        if params[:category].present?
+            'posts/branch/search_form/category_field'
+        else
+        'shared/empty_partial'
+        end
     end
-  end
+
+  
 
 end
